114c85a1a1b7e039df915ea815b0cfef994cb7aa
Jodie Gardiner <jodiegardiner@outlook.com>
2023-07-03 15:08:24 +0100

    can we

--- app.py
+++ app.py
@@ -9,22 +9,24 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key_here'  # Replace with your secret key
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///events.db'  # Replace with your database URI
 
-# Initialize the database
+# Initialize the database and other extensions
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 
 
-# User model for authentication
+# User class for managing users and login sessions
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(100), unique=True, nullable=False)
-    password = db.Column(db.String(100), nullable=False)
-    events = db.relationship('Event', backref='author', lazy=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.username}')"
 
 
-# Event model
+# Event class for managing events
 class Event(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
@@ -39,21 +41,28 @@ class Event(db.Model):
         return f"Event('{self.title}', '{self.date}')"
 
 
+# Create the database tables before the first request
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+
+# Dummy admin user for demonstration purposes
+def create_admin():
+    admin = User(username='admin', password=bcrypt.generate_password_hash('admin').decode('utf-8'))
+    db.session.add(admin)
+    db.session.commit()
+
+
+# Initialize the login manager to handle user sessions
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
 
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-
+# Routes for login and events
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    if current_user.is_authenticated:
-        return redirect(url_for('events'))
-
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
@@ -61,9 +70,10 @@ def login():
         user = User.query.filter_by(username=username).first()
         if user and bcrypt.check_password_hash(user.password, password):
             login_user(user)
-            return redirect(url_for('events'))
+            flash('Login successful!', 'success')
+            return redirect(url_for('index'))
         else:
-            flash('Invalid username or password', 'danger')
+            flash('Login failed. Please check your username and password.', 'error')
 
     return render_template('login.html')
 
@@ -72,63 +82,29 @@ def login():
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for('home'))
+    flash('You have been logged out.', 'info')
+    return redirect(url_for('login'))
 
 
-@app.route('/events')
+@app.route('/')
 @login_required
-def events():
-    events = Event.query.filter_by(user_id=current_user.id).all()
-    return render_template('events.html', events=events)
+def index():
+    return render_template('index.html', events=current_user.events)
 
 
-@app.route('/events/add', methods=['GET', 'POST'])
+# Dummy route to add events (you can modify this to handle event creation)
+@app.route('/add_event')
 @login_required
 def add_event():
-    if request.method == 'POST':
-        title = request.form['title']
-        description = request.form['description']
-
-        event = Event(title=title, description=description, author=current_user)
-        db.session.add(event)
-        db.session.commit()
-
-        flash('Event added successfully', 'success')
-        return redirect(url_for('events'))
-
-    return render_template('add_event.html')
+    event = Event(title='Sample Event', description='This is a sample event.', user_id=current_user.id)
+    db.session.add(event)
+    db.session.commit()
+    flash('Event added successfully!', 'success')
+    return redirect(url_for('index'))
 
 
-@app.route('/events/delete/<int:event_id>', methods=['POST'])
-@login_required
-def delete_event(event_id):
-    event = Event.query.get(event_id)
-    if event:
-        db.session.delete(event)
-        db.session.commit()
-        flash('Event deleted successfully', 'success')
-
-    return redirect(url_for('events'))
-
-
-# Create the admin user and insert it into the database
-def create_admin():
-    admin_username = "admin"  # Replace with your desired admin username
-    admin_password = "admin123"  # Replace with your desired admin password
-
-    # Check if the admin user already exists
-    admin = User.query.filter_by(username=admin_username).first()
-    if not admin:
-        hashed_password = bcrypt.generate_password_hash(admin_password).decode('utf-8')
-        admin = User(username=admin_username, password=hashed_password)
-        db.session.add(admin)
-        db.session.commit()
-
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     with app.app_context():
-        db.create_all()
-        create_admin()  # Call the function to create the admin user
+        create_admin()  # Create the admin user if it doesn't exist
 
     app.run(debug=True)
